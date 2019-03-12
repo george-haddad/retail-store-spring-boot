1e358d53189f9459d635faac577d557560606b01
George Haddad <george.haddad@postlight.com>
2019-03-12 12:09:17 +0200

    feat: use non-deprecated ApiInfo class

--- src/main/java/com/retail/demo/DemoApplication.java
+++ src/main/java/com/retail/demo/DemoApplication.java
@@ -1,11 +1,16 @@
 package com.retail.demo;
 
+import java.util.Collection;
+import java.util.HashSet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.StringVendorExtension;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,9 +33,19 @@ public class DemoApplication {
                 .apiInfo(generateApiInfo());
     }
 
-    @SuppressWarnings("deprecation")
     private ApiInfo generateApiInfo() {
-        return new ApiInfo("retail demo server", "", "Version 1.0","urn:tos", "hello@retail-demo.com", "Unlicense", "https://tldrlegal.com/license/unlicense");
+        @SuppressWarnings("rawtypes")
+        Collection<VendorExtension> extensions = new HashSet<>();
+        extensions.add(new StringVendorExtension("vendor", "retail-demo"));
+        
+        return new ApiInfo("Retail Demo REST API",
+                           "REST API documentaiton for the Retail Demo",
+                           "Version 1.0",
+                           "urn:tos",
+                           new Contact("Retail Coding Services", "https://retail.demo.com/", "hello@retail.demo.com"),
+                           "Unlicense",
+                           "https://tldrlegal.com/license/unlicense",
+                           extensions);
     }
 }
 
