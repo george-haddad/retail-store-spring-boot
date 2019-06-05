ee8c5d79a6364fd883d22b1d59baee3c493e2c43
George Haddad <george.haddad@postlight.com>
2019-06-05 15:25:49 +0300

    feat: spring environment var

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
 COPY ${DEPENDENCY}/BOOT-INF/classes /app
 COPY ${DEPENDENCY}/META-INF /app/META-INF
 
+ENV SPRING_PROFILES_ACTIVE=dev 
 ENTRYPOINT ["java", "-cp", "app:app/lib/*", "com.retail.store.RetailStoreApplication"]
---
+++ src/main/java/com/retail/store/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package com.retail.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+@Profile({ "dev" })
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        return filter;
+    }
+}
